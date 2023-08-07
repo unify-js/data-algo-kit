d27c1d3e9f643ac106219700b65328e420007675
luohuidong <luohuidong01@126.com>
2023-08-08 00:12:24 +0800

    chore: project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+export default {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+};
---
+++ README.md
@@ -0,0 +1,24 @@
+# Node TypeScript Template
+
+## How to use
+
+### Development
+
+```bash
+pnpm dev
+```
+
+### Production
+
+```bash
+pnpm build
+```
+
+### Testing
+
+Before running tests, make sure you have built the project.
+
+```bash
+pnpm dev # or pnpm build
+pnpm test # or pnpm test:watch
+```
