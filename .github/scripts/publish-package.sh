099f93f7d8ac4104d586da6c653268bbb59d3a22
luohuidong <luohuidong01@126.com>
2023-09-22 22:38:41 +0800

    ci: add "release" GitHub Actions workflow

---
+++ .github/scripts/publish-package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./publish-package.sh
+
+pnpm install || utils::check_fail $? "pnpm install failed"
+pnpm test || utils::check_fail $? "test failed"
+
+pnpm changeset version
+
+is_modify=$(git status --porcelain)
+
+if [ ! -z "$is_modify" ]; then
+    echo "packages version changed"
+
+    git config user.name "luohuidong"
+    git config user.email "luohuidong01@126.com"
+    git add .
+    git commit -m "build: update packages version"
+
+    pnpm release || utils::check_fail $? "release failed"
+
+    git push || utils::check_fail $? "git push failed"
+else
+    echo "packages version not changed"
+fi
